0b30c55da9a07a300874b6eec53060d2ecd4e687
wait-yyy <1298055690@qq.com>
2026-05-14 01:50:12 +0800

    vault backup: 2026-05-14 01:50:12

--- src/config/profileConfig.ts
+++ src/config/profileConfig.ts
@@ -9,10 +9,10 @@ export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/avatar.avif",
 
 	// 名字
-	name: "Firefly",
+	name: "影颜",
 
 	// 个人签名
-	bio: "Hello, I'm Firefly.",
+	bio: "行至归处，不知来路",
 
 	// 链接配置
 	// 已经预装的图标集：fa7-brands，fa7-regular，fa7-solid，material-symbols，simple-icons
